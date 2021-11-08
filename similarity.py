b26ad37e5bccbb8deef1f051417d0241e3bfba6a
Hong <64223259+HongSungRae@users.noreply.github.com>
2021-11-08 17:38:11 +0900

    Create similarity.py

---
+++ similarity.py
@@ -0,0 +1,13 @@
+'''
+This module verifies that the trained model classifies
+the presentation of POP music well.
+'''
+
+
+### 1
+# 50곡 넣어서 50x50 similarity뽑기
+# 다양한 measure 쓸 수 있게 하기
+
+
+### 2
+# 아티스트별로 구분하여 비슷한 아티스트끼리 묶기
