849eab10265a520d764667f64838d2adfe886840
Hong <64223259+HongSungRae@users.noreply.github.com>
2021-11-08 23:13:45 +0900

    Update augmentation.py

--- augmentation.py
+++ augmentation.py
@@ -1,13 +1,13 @@
+# https://pytorch.org/audio/stable/functional.html#dither
+
 import torchaudio
+import numpy as np
 from torchaudio.functional import gain, flanger, overdrive, phaser, dither
 
 
-def get_aumentation():
-    pass
-
-
-def listen():
-    pass
+def get_aumentation(audio):
+    rand = np.random.random(1)
+    return aug_audio
 
 
 if __name__ == '__main__':
