dc6547e3bd8c9e08effff207c2d09b4ab8f76531
Hong <64223259+HongSungRae@users.noreply.github.com>
2021-11-08 21:05:53 +0900

    Create augmentation.py

---
+++ augmentation.py
@@ -0,0 +1,14 @@
+import torchaudio
+from torchaudio.functional import gain, flanger, overdrive, phaser, dither
+
+
+def get_aumentation():
+    pass
+
+
+def listen():
+    pass
+
+
+if __name__ == '__main__':
+    pass
