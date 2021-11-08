b8654a7fe65339c004870733df8b233c29dd0650
Hong <64223259+HongSungRae@users.noreply.github.com>
2021-11-08 21:06:02 +0900

    Update main.py

---
+++ main.py
@@ -0,0 +1,27 @@
+# library
+import torch
+from torch.utils.data import DataLoader
+import parser
+
+# local
+from dataset import MTA, GTZAN
+
+
+def train():
+    pass
+
+
+def validation():
+    pass
+
+
+def test():
+    pass
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
